14678f9f9df0c5d2c53306f5a9971c17a222562c
Anton Miroshkin <anton.miroshkin@gmail.com>
2019-11-11 17:31:08 +0300

    Added start to localization

--- DzrGarage/DzrGarage/Startup.cs
+++ DzrGarage/DzrGarage/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
 using DzrGarage.Data;
 using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -36,6 +37,12 @@ namespace DzrGarage
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
+            
+            services.AddMvc()
+                .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
+                .AddDataAnnotationsLocalization();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
